7016a7dc09db09d5892d80c43dfdb7c49f01f2cd
Mark Kraemer <megamarker@gmail.com>
2016-09-15 10:14:13 -0400

    Sample data setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import emails from '../data/emails';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      emails: emails
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
