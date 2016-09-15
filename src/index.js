8b99d9d0c762ebae1dee4f14150e5e78dd1da5ce
Mark Kraemer <megamarker@gmail.com>
2016-09-15 11:19:48 -0400

    Remove Atellier

--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import ReactAtellier from 'react-atellier';
 import App from './components/App';
-import Email from './components/Email';
 import './index.css';
-
-const componentList = [{
-  componentName : 'Email',
-  component : Email,
-  email: {from: 'asdf', content: 'asoidfoqbpibegiubeqrg'}
-}];
-
-var AtellierWrapper = React.createClass({
-  render: function() {
-    console.log('here')
-    return (
-      <ReactAtellier components={componentList} />
-    );
-  }
-});
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
-    </Route>
-    <Route path="/atellier" component={AtellierWrapper}/>
+    <Route path="/" component={App}></Route>
   </Router>
 )
 
 ReactDOM.render(routes, document.getElementById('root'));
-
-// <Route path="atellier" component={AtellierWrapper}/>
