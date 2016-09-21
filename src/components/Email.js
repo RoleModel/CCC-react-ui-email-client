3a15e0a3dc2b6cf52fa28a0d989d8026f4a91557
Mark Kraemer <megamarker@gmail.com>
2016-09-21 16:22:32 -0400

    Fix swipeable issue.
    swipeble needs to be render inside the item that is removed (ListItem),
    otherwise it doesn't swipe correctly for some reason.

--- src/components/Email.js
+++ src/components/Email.js
@@ -7,29 +7,6 @@ import FlatButton from 'material-ui/FlatButton';
 
 class Email extends Component {
 
-  renderEmail() {
-    const email = this.props.email
-    return (
-      <div>
-        <ListItem leftAvatar={<Avatar src={this.props.email.profile} />} onClick={this.props.toggleDisplay.bind(null, this.props.email)}>
-          <span className="email-from">{email.from}</span>
-          <span className="email-subject">{email.subject}</span>
-          { !this.props.isTouch ? this.renderButtons.bind(this)() : null }
-          { email.isShown ? <p ref="content">{email.content}</p> : null }
-        </ListItem>
-        <Divider />
-      </div>
-    )
-  }
-
-  renderTouch() {
-    return (
-      <Swipeable onSwipedLeft={this.props.deleteEmail.bind(null, this.props.email)} >
-        {this.renderEmail.bind(this)()}
-      </Swipeable>
-    )
-  }
-
   renderButtons() {
     return (
       <div style={{float: 'right'}}>
@@ -40,14 +17,21 @@ class Email extends Component {
   }
 
   render() {
-    if(this.props.isTouch) {
-      return this.renderTouch.bind(this)()
-    } else {
-      return this.renderEmail.bind(this)()
-    }
+    const email = this.props.email
+    return (
+      <div>
+        <ListItem leftAvatar={<Avatar src={this.props.email.profile} />} onClick={this.props.toggleDisplay.bind(null, this.props.email)}>
+          <Swipeable onSwipedLeft={this.props.deleteEmail.bind(null, this.props.email)} >
+            <span className="email-from">{email.from}</span>
+            <span className="email-subject">{email.subject}</span>
+            { !this.props.isTouch ? this.renderButtons.bind(this)() : null }
+            { email.isShown ? <p ref="content">{email.content}</p> : null }
+          </Swipeable>
+        </ListItem>
+        <Divider />
+      </div>
+    )
   }
 }
 
 export default Email;
-
-// "start": "npm run compile-sass && npm run background-sass-watcher && react-scripts start && kill $WATCH_SASS_PID",
